d215aad55b4a2d4cb2ae57e492fa83962d72a019
Andrey Mikhaylin <cenobith@gmail.com>
2016-02-04 21:04:44 +0300

    Added Static Pages and basic layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #def set_locale
+  #  I18n.locale = params[:locale] || I18n.default_locale
+  #end
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+#  def after_sign_in_path_for(resource_or_scope)
+#    if resource_or_scope.is_a?(User)
+#      if current_user.profile.nil?
+#        new_profile_path
+#      else
+#        super
+#      end
+#    else
+#      super
+#    end
+#  end
+
+  private
+  def extract_locale_from_accept_language_header
+    if !request.env['HTTP_ACCEPT_LANGUAGE'].nil?
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    else
+      return 'en'
+    end
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+  before { @user = User.new(email: "user@example.com",
+                            password: "testtest", password_confirmation: "testtest") }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid}
+  it { should_not be_admin}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
