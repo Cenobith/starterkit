680e8a4cfc470213b70fe149eedf993b3b826938
Andrey Mikhaylin <cenobith@gmail.com>
2017-02-09 13:19:06 +0300

    First look: show modal login dialog, if it fails - redirect to default devise login page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -29,9 +28,8 @@ ActiveRecord::Schema.define(version: 20160204180940) do
     t.string   "userpic_content_type"
     t.integer  "userpic_file_size"
     t.datetime "userpic_updated_at"
+    t.index ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
   end
-
-  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -47,9 +45,8 @@ ActiveRecord::Schema.define(version: 20160204180940) do
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
